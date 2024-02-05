ca28d7dd2c54d308d89cd62842b47a4a31d8c9f9
MostofaFattha <mostofacse14@gmail.com>
2024-02-05 21:00:30 +0600

    findPositionsOfChar

---
+++ .vscode/ArrayList.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        String[] words = { "abc","bcd","aaaa","cbc"};
+        char x = 'z';
+        int[] result = findPositionsOfChar(words, x);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] findPositionsOfChar(String[] words, char x) {
+        int[] result = new int[words.length];
+        int index = 0;
+
+        for (int wordIndex = 0; wordIndex < words.length; wordIndex++) {
+            String word = words[wordIndex];
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == x) {
+                    result[index++] =wordIndex ;
+                    break;
+                }
+            }
+        }
+
+        return Arrays.copyOf(result, index);
+    }
+}
