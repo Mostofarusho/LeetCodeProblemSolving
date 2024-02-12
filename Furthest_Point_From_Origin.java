d2b11cb5852ec87ab20127fc790634a1022a2e44
MostofaFattha <mostofacse14@gmail.com>
2024-02-12 22:09:41 +0600

    furthestDistanceFromOrigin

---
+++ Furthest_Point_From_Origin.java
@@ -0,0 +1,37 @@
+public class Furthest_Point_From_Origin {
+
+    public static void main(String[] args) {
+        String moves = "L_RL__R";
+        int ans = furthestDistanceFromOrigin(moves);
+        System.out.println(ans);
+    }
+
+    static int furthestDistanceFromOrigin(String moves) {
+        char move[] = moves.toCharArray();
+        int x = 0;
+        int left = 0, right = 0;
+        for (int i = 0; i < move.length; i++) {
+            if (move[i] == 'L') {
+                x--;
+                left++;
+            } else if (move[i] == 'R') {
+                x++;
+                right++;
+            }
+        }
+        for (int j = 0; j < move.length; j++) {
+            if (move[j] == '_') {
+                if(left==right){
+                    x--;
+                }
+                else if (left > right) {
+                    x--;
+                } else if (left < right) {
+                    x++;
+                }
+            }
+
+        }
+        return Math.abs(x);
+    }
+}
