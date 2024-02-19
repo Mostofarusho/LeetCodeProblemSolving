83d56e7141e2c06ba3a7c18c947b51b42bc138a9
MostofaFattha <mostofacse14@gmail.com>
2024-02-19 18:52:15 +0600

    FirstRepet

---
+++ repetedchar.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class repetedchar {
+    public static void main(String[] args) {
+        char ans = repeatedCharacter("abccbaacz");
+        System.out.println(ans);
+    }
+
+    static char repeatedCharacter(String s) {
+
+        char ch = ' ';
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                ch = s.charAt(i);
+                break;
+            }
+            map.put(s.charAt(i), i);
+        }
+        return ch;
+    }
+}
