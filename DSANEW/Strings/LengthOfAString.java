dc7469895ef292c3a7e8058d1ff7066d4da70897
MostofaFattha <mostofacse14@gmail.com>
2025-05-22 13:38:41 +0600

    Length Of A String

---
+++ DSANEW/Strings/LengthOfAString.java
@@ -0,0 +1,7 @@
+public class LengthOfAString {
+    public static void main(String[] args) {
+        String myString="ABC TECH";
+        char[] ch=myString.toCharArray();
+        System.out.println(ch.length);
+    }
+}
