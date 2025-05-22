26f165fa44dd89a2ac2238cc7bab90f0a04a5049
MostofaFattha <mostofacse14@gmail.com>
2025-05-22 14:05:36 +0600

    Length Of A String Updated

--- DSANEW/Strings/LengthOfAString.java
+++ DSANEW/Strings/LengthOfAString.java
@@ -1,7 +1,14 @@
 public class LengthOfAString {
     public static void main(String[] args) {
         String myString="ABC TECH";
-        char[] ch=myString.toCharArray();
-        System.out.println(ch.length);
+        myString=myString.concat("\0");
+        char ch[]=myString.toCharArray();
+        int count=0;
+        int i=0;
+        while(ch[i]!='\0'){
+            ++count;
+            ++i;
+        }
+        System.out.println("%d"+count);
     }
 }
