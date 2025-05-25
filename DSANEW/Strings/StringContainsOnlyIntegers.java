150e89489781e32af9bc35db4af9b800f4d143fd
MostofaFattha <mostofacse14@gmail.com>
2025-05-25 12:55:09 +0600

    Modified

--- DSANEW/Strings/StringContainsOnlyIntegers.java
+++ DSANEW/Strings/StringContainsOnlyIntegers.java
@@ -1,11 +1,10 @@
 public class StringContainsOnlyIntegers {
     public static void main(String[] args) {
-        String myString="999A99";
-        char[] ch=myString.toCharArray();
-        int size=ch.length;
+        String myString="99999";
+        int size=myString.length();
         int i=0;
         while (i!=size) { 
-            if(ch[i]>=0 && ch[i]<='9'){
+            if(myString.charAt(i)>=0 && myString.charAt(i)<='9'){
                 i++;
             }
             else{
