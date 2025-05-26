81cb56a55a8441ac3fdf2844a0c3c0450a0de367
MostofaFattha <mostofacse14@gmail.com>
2025-05-26 12:13:11 +0600

    ForEach LOOP

---
+++ DSANEW/Strings/LoopsInString.java
@@ -0,0 +1,10 @@
+public class LoopsInString {
+    public static void main(String[] args) {
+        int[] myString={10,20,30,40,50};
+        int sum=0;
+        for(int data:myString){
+            sum=sum+data;
+        }
+        System.out.println(sum);
+    }
+}
