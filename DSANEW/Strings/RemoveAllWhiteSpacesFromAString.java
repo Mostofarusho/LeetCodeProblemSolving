25fe037c70f6a308e01307dbbc3cd3efd4d0f6d0
MostofaFattha <mostofacse14@gmail.com>
2025-05-27 12:29:30 +0600

    StringsAreImmutable

--- DSANEW/Strings/RemoveAllWhiteSpacesFromAString.java
+++ DSANEW/Strings/RemoveAllWhiteSpacesFromAString.java
@@ -5,3 +5,4 @@ public class RemoveAllWhiteSpacesFromAString {
         System.out.println(newString);
     }
 }
+ 
---
+++ DSANEW/Strings/StringsAreImmutable.java
@@ -0,0 +1,9 @@
+public class StringsAreImmutable {
+    public static void main(String[] args) {
+        StringBuffer x=new StringBuffer("Mostofa");
+        StringBuffer y=new StringBuffer("Rusho");
+        x.append(y);
+        System.out.println(x);
+    }
+    
+}
