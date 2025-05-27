0d695b905b0299e95080a939c25132fcddf843ec
MostofaFattha <mostofacse14@gmail.com>
2025-05-27 12:40:33 +0600

    SwapTwoStrings

---
+++ DSANEW/Strings/SwapTwoStrings.java
@@ -0,0 +1,19 @@
+public class SwapTwoStrings {
+    public static void main(String[] args) {
+    StringBuffer x=new StringBuffer("Mostofa");
+    StringBuffer y=new StringBuffer("Rusho");
+    StringBuffer z=new StringBuffer("");
+    System.out.println(y+" "+x);
+    z=y;
+    y=x;
+    x=z;
+    System.out.println(y+" "+x);
+    // String x="Mostofa";
+    // String y="Rusho";
+    // String z="";
+    // z=y;
+    // y=x;
+    // x=z;
+    // System.out.println(y+" "+x);
+    }
+}
