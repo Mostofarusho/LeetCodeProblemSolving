38c0b54d932729f41ddee5e35453b4f5522aa276
MostofaFattha <mostofacse14@gmail.com>
2025-05-27 11:24:46 +0600

    RemoveDuplicateCharactersInAString

---
+++ DSANEW/Strings/RemoveDuplicateCharactersInAString.java
@@ -0,0 +1,32 @@
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RemoveDuplicateCharactersInAString {
+    public static void main(String[] args) {
+         String myString="SPIDER_WEBS_OF_LIES";
+         char ch[]=myString.toCharArray();
+         int size=ch.length;
+         int i=0;
+         Map<Character,Integer> map=new LinkedHashMap<>();
+         while (i!=size) { 
+             if(map.containsKey(ch[i])==false){
+                map.put(ch[i], 1);
+             }
+             else{
+                int oldVal=map.get(ch[i]);
+                int newVal=oldVal+1;
+                map.put(ch[i], newVal);
+             }
+             i++;
+         }
+         String result="";
+         for (Map.Entry<Character, Integer> data : map.entrySet()) {
+             if(data.getValue()==1){
+                result=result+data.getKey();
+             }
+             
+         }
+         System.out.println(result);
+    }
+}
