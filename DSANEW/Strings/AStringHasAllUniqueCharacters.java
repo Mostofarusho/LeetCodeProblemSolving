4c7bb97047e4e9db806eaa66a7485c80de7f56b7
MostofaFattha <mostofacse14@gmail.com>
2025-05-27 09:58:53 +0600

    AStringHasAllUniqueCharacters

---
+++ DSANEW/Strings/AStringHasAllUniqueCharacters.java
@@ -0,0 +1,32 @@
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class AStringHasAllUniqueCharacters {
+    public static void main(String[] args) {
+        String givenString="SILLYSPIDERS";
+        char[] ch=givenString.toCharArray();
+        int size=ch.length;
+        Map<Character,Integer> map=new LinkedHashMap<>();
+        int i=0;
+        while (i!=size) { 
+            if(map.containsKey(ch[i])==false){
+                map.put(ch[i], 1);
+            }
+            else{
+                int oldVal=map.get(ch[i]);
+                int newVal=oldVal+1;
+                map.put(ch[i], newVal);
+            }
+            i++;
+        }
+        for(Map.Entry<Character,Integer> data:map.entrySet()){
+            if(data.getValue()>1){
+                System.out.println("Does not contain All Unique Characters ");
+                System.exit(0);
+            }
+            
+        }
+        System.out.println("Does contain All Unique Characters ");
+    }
+}
